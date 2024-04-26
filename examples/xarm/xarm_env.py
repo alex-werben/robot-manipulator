678d874e9ef0431b8bc3e86701923186af2399de
Alex_Werben <crystall.werben@gmail.com>
2024-04-26 19:00:36 +0300

    experiments with gripper

--- examples/xarm/xarm_env.py
+++ examples/xarm/xarm_env.py
@@ -9,47 +9,48 @@ class Env:
 	def __init__(self, rts=1):
 
 		p.connect(p.GUI)  # , options="--background_color_red=1.0 --background_color_blue=1.0 --background_color_green=1.0")
-		# p.connect()
 		p.setAdditionalSearchPath(pd.getDataPath())
 		print(pd.getDataPath())
 		self.REAL_TIME_SIMULATION_FLAG = rts
 		useFixedBase = True
-		# flags = p.URDF_INITIALIZE_SAT_FEATURES#0#
-		# flags = p.URDF_USE_SELF_COLLISION
 		flags = 0
 
-		# plane_pos = [0,0,0]
-		# plane = p.loadURDF("plane.urdf", plane_pos, flags = flags, useFixedBase=useFixedBase)
+		plane_pos = [0,0,0]
+		plane = p.loadURDF("plane.urdf", plane_pos, flags = flags, useFixedBase=useFixedBase)
 		table_pos = [0, 0, -0.625]
 		self.tableId = p.loadURDF("table/table.urdf", table_pos, flags=flags, useFixedBase=useFixedBase)
-		# self.xarmId = p.loadURDF("xarm/xarm6_robot.urdf", flags=flags, useFixedBase=useFixedBase)
-		self.xarmId = p.loadURDF("xarm/xarm6_robot_white.urdf", [0, 0, 0])
+		self.xarmId = p.loadURDF("xarm/xarm6_robot.urdf", flags=flags, useFixedBase=useFixedBase)
+		# self.xarmId = p.loadURDF("xarm/xarm6_with_gripper.urdf", [0, 0, 0])
 
-	# self.xarmId = p.loadURDF("kuka_iiwa/model.urdf", [0, 0, 0])
 
 	def get_info(self):
 		self.jointIds = []
 		self.paramIds = []
-		# self.numJoints = p.getNumJoints(self.xarmId)
 		rp = [0, 0.5 * math.pi, 0, 0, 0, 0, 0]
 		for i in range(p.getNumJoints(self.xarmId)):
 			p.resetJointState(self.xarmId, i, rp[i])
 		self.gripperId = 6
 		self.lastJointId = p.getNumJoints(self.xarmId) - 1
-
 		self.movableJoints = 0
+
+		self.gripperJointIds = []
 
 		for j in range(p.getNumJoints(self.xarmId)):
 			# p.changeDynamics(self.xarmId, j, linearDamping=0, angularDamping=0)
 			info = p.getJointInfo(self.xarmId, j)
 
-			jointName = info[1]
+			jointName: str = info[1]
 			jointType = info[2]
 
-			if jointType == p.JOINT_PRISMATIC or jointType == p.JOINT_REVOLUTE:
+			if jointName.startswith("joint") and (jointType == p.JOINT_PRISMATIC or jointType == p.JOINT_REVOLUTE):
 				self.jointIds.append(j)
 				self.paramIds.append(p.addUserDebugParameter(jointName.decode("utf-8"), -4, 4, 0))
 				self.movableJoints += 1
+
+			if jointName.endswith("joint") and (jointType == p.JOINT_PRISMATIC or jointType == p.JOINT_REVOLUTE):
+				self.gripperJointIds.append(j)
+
+
 
 	# print(p.getLinkState(self.xarmId, self.gripperId))
 
@@ -111,56 +112,33 @@ class Env:
 	def simulate(self):
 		p.setRealTimeSimulation(0)
 		self.trailDuration = 50
+
+		jointPoses = p.calculateInverseKinematics(self.xarmId,
+													self.gripperJointIds[-1],
+													[0.3, 0.3, 0.3],
+													p.getQuaternionFromEuler([0, -math.pi, 0]))
+		print(jointPoses)
 		# pos = [0, 0.1, 0.05]
 		# prevPose = pos.copy()
-		positions = [
-			# [0, 0.2, 0.05],
-			[0, 0.3, 0.1],
-			[0.3, 0.3, 0.1],
-			[0.3, 0.3, 0.3],
-			[-0.3, 0.3, 0.3],
-			[-0.3, -0.3, 0.3],
-			[0.3, -0.3, 0.3],
-			[0, 0.3, 0.3],
-			[0, 0.3, 0.1],
-		]
-		for pos in positions:
-			self.move_to_position(pos)
+		# positions = [
+		# 	# [0, 0.2, 0.05],
+		# 	[0, 0.3, 0.1],
+		# 	[0.3, 0.3, 0.1],
+		# 	[0.3, 0.3, 0.3],
+		# 	[-0.3, 0.3, 0.3],
+		# 	[-0.3, -0.3, 0.3],
+		# 	[0.3, -0.3, 0.3],
+		# 	[0.3, 0.3, 0.3],
+		# ]
+		# for pos in positions:
+		# 	self.move_to_position(pos)
 		#
-		# # directions = [1, 1, 0, 2, 0, 2]
-		# prevLinkPose = p.getLinkState(self.xarmId, self.gripperId)[0]
-		#
-		# while pos[1] < 0.3:
-		# 	pos[1] += 0.01
-		# 	jointPoses = self.calculate_joint_poses(pos)
-		# 	for j in range(self.movableJoints):
-		# 		p.setJointMotorControl2(bodyIndex=self.xarmId,
-		# 														jointIndex=j + 1,
-		# 														controlMode=p.POSITION_CONTROL,
-		# 														targetPosition=jointPoses[j],
-		# 														# targetVelocity=0,
-		# 														force=5 * 240.,
-		# 														# positionGain=0.03,
-		# 														# velocityGain=1
-		# 														)
-		# 	linkPose = p.getLinkState(self.xarmId, self.gripperId)[0]
-		# 	p.addUserDebugLine(prevLinkPose, linkPose, [0.7, 0, 0], 1, self.trailDuration)
-		# 	p.addUserDebugLine(prevPose, pos, [0, 0, 0.3], 1, self.trailDuration)
-		# 	prevLinkPose = linkPose
-		# 	prevPose = pos.copy()
-		# 	time.sleep(1. / 240.)
-		# 	print(p.getLinkState(self.xarmId, self.gripperId)[0])
-
-
-
-		# p.addUserDebugLine(prevPose, pos, [0, 0, 0.3], 1, trailDuration)
-
-		p.disconnect()
+		# p.disconnect()
 
 
 if __name__ == "__main__":
 	env = Env()
 	env.get_info()
 	# env.calculate_joint_poses()
-	env.simulate()
+	env.simulate_debug()
 
--- examples/xarm/xarm_example.py
+++ examples/xarm/xarm_example.py
@@ -8,21 +8,24 @@ print(pd.getDataPath())
 
 useFixedBase = True
 # flags = p.URDF_INITIALIZE_SAT_FEATURES#0#
-# flags = p.URDF_USE_SELF_COLLISION
-flags = 0
+flags = p.URDF_USE_SELF_COLLISION
+# flags = 0
 
 # plane_pos = [0,0,0]
 # plane = p.loadURDF("plane.urdf", plane_pos, flags = flags, useFixedBase=useFixedBase)
 table_pos = [0, 0, -0.625]
-table = p.loadURDF("table/table.urdf", table_pos, flags=flags, useFixedBase=useFixedBase)
-xarm = p.loadURDF("xarm/xarm6_robot.urdf", flags=flags, useFixedBase=useFixedBase)
-
+# table = p.loadURDF("table/table.urdf", table_pos, flags=flags, useFixedBase=useFixedBase)
+xarm = p.loadURDF("xarm/xarm6_with_gripper.urdf", flags=flags, useFixedBase=useFixedBase)
+# p.loadSDF()loadSDF
+#
 jointIds = []
 paramIds = []
 gripperId = -1
 
+print(xarm)
+# xarm = xarm[0]
 for j in range(p.getNumJoints(xarm)):
-    p.changeDynamics(xarm, j, linearDamping=0, angularDamping=0)
+    # p.changeDynamics(xarm, j, linearDamping=0, angularDamping=0)
     info = p.getJointInfo(xarm, j)
     # print(info)
     jointName = info[1]
@@ -30,13 +33,13 @@ for j in range(p.getNumJoints(xarm)):
 
     if jointType == p.JOINT_PRISMATIC or jointType == p.JOINT_REVOLUTE:
         jointIds.append(j)
-        paramIds.append(p.addUserDebugParameter(jointName.decode("utf-8"), -4, 4, 0))
+        paramIds.append(p.addUserDebugParameter(jointName.decode("utf-8"), -3.14, 3.14, 0))
 
 skip_cam_frames = 10
 print(jointIds)
 j = 1
 while True:
-    print(p.getLinkState(xarm, 6)[0], j)
+    # print(p.getLinkState(xarm, 6)[0], j)
     j += 1
     p.stepSimulation()
     for i in range(len(paramIds)):
