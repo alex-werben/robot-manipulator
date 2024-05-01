000ad41afef02ae6627966f1168543fb70eea12d
alexwerben <crystall.werben@gmail.com>
2024-05-01 19:56:39 +0300

    env improve, model found

--- env.py
+++ env.py
@@ -1,38 +1,56 @@
-import pybullet as p
-import time
-import pybullet_data
-import random, numpy, math, time
-import matplotlib.pyplot as plt
-from matplotlib import animation
-from IPython.display import display, HTML
+import math
 import os
+import time
+import numpy as np
+import gym
+from gym import spaces
+import pybullet as p
+import pybullet_data
+import random
 
 useNullSpace = 1
 ikSolver = 0
-pandaEndEffectorIndex = 11 #8
+pandaEndEffectorIndex = 11
 pandaNumDofs = 7
 ll = [-7]*pandaNumDofs
 #upper limits for null space (todo: set them to proper range)
 ul = [7]*pandaNumDofs
 #joint ranges for null space (todo: set them to proper range)
 jr = [7]*pandaNumDofs
-jointPositions=[0.98, 0.458, 0.31, -2.24, -0.30, 2.66, 2.32, 0.02, 0.02]
+jointPositions = [0.98, 0.458, 0.31, -2.24, -0.30, 2.66, 2.32, 0.02, 0.02]
 
 rp = jointPositions
-class Environment:
+
+
+class Environment(gym.Env):
     def __init__(self):
         p.connect(p.GUI)
+        self.reset()
+
+        self.action_space = spaces.Box(low=np.array([0.1, -0.3, 0.5]),
+                                       high=np.array([0.7, 0.3, 0.9]))
+        self.observation_space = spaces.Box(low=np.array([0.1, -0.3, 0.5]),
+                                            high=np.array([0.7, 0.3, 0.9]))
+
+
+
+    def reset(self, seed=23):
+        p.resetSimulation()
+        self.t = 0
+        p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
-        p.setGravity(0, 0, -1)
+        p.setGravity(0, 0, -9.81)
+        p.setPhysicsEngineParameter(solverResidualThreshold=0)
+
         orientation = p.getQuaternionFromEuler([0, 0, 0])
         self.panda = p.loadURDF("franka_panda/panda.urdf",
                                 useFixedBase=True,
                                 basePosition=[0, 0, 0.5],
                                 baseOrientation=orientation)
         self.plane = p.loadURDF("plane.urdf")
-        self.lego = p.loadURDF("lego/lego.urdf", basePosition=[0.5, 0.5, 0.6])
-        self.gripper_height = 0.2 + 0.5
+        # self.lego = p.loadURDF("lego/lego.urdf", basePosition=[0.5, 0, 0.6])
+        self.gripper_height = 0.2 + 0.7
         self.state = 0
         self.control_dt = 1. / 240.
         self.finger_target = 0
@@ -47,6 +65,7 @@ class Environment:
                                childFramePosition=[0, 0, 0])
         p.changeConstraint(c, gearRatio=-1, erp=0.1, maxForce=50)
         p.setAdditionalSearchPath(os.getcwd())
+        index = 0
 
         self.surface = p.loadURDF("assets/cube.urdf", basePosition=[0.25, 0, 0.25], useFixedBase=True)
         self.end_position = p.loadURDF("assets/position.urdf", basePosition=[0.6, 0.5, 0.525], useFixedBase=True)
@@ -54,8 +73,8 @@ class Environment:
         p.changeVisualShape(self.end_position, -1, rgbaColor=[0, 1, 0, 0.3])
         p.changeVisualShape(self.start_position, -1, rgbaColor=[1, 0, 0, 0.3])
         self.border = p.loadURDF("assets/border.urdf", basePosition=[0.25, 0, 0.525], useFixedBase=True)
-
-        index = 0
+        state_object = [random.uniform(0.3, 0.6), random.uniform(-0.4, 0.4), 0.6]
+        self.lego = p.loadURDF("assets/block.urdf", state_object)
 
         for j in range(p.getNumJoints(self.panda)):
             p.changeDynamics(self.panda, j, linearDamping=0, angularDamping=0)
@@ -63,16 +82,21 @@ class Environment:
             # print("info=",info)
             jointName = info[1]
             jointType = info[2]
-            if (jointType == p.JOINT_PRISMATIC):
+            if jointType == p.JOINT_PRISMATIC:
                 p.resetJointState(self.panda, j, jointPositions[index])
                 index = index + 1
-            if (jointType == p.JOINT_REVOLUTE):
+            if jointType == p.JOINT_REVOLUTE:
                 p.resetJointState(self.panda, j, jointPositions[index])
                 index = index + 1
-        self.t = 0.
+        p.addUserDebugText('X', [1, 0, 0.5], [0, 0, 0])
+        p.addUserDebugText('Y', [0, 1, 0.5], [0, 0, 0])
 
-    def reset(self):
-        pass
+        state_robot = p.getLinkState(self.panda, 11)[0]
+        state_fingers = (p.getJointState(self.panda, 9)[0], p.getJointState(self.panda, 10)[0])
+        observation = state_robot + state_fingers
+        p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
+
+        return observation
 
     def update_state(self):
         keys = p.getKeyboardEvents()
@@ -91,51 +115,110 @@ class Environment:
                         self.state = 5
                     if k == ord('6'):
                         self.state = 6
+                    if k == ord('7'):
+                        self.state = 7
+                    if k == ord('8'):
+                        self.state = 8
+
                 if v & p.KEY_WAS_RELEASED:
                     self.state = 0
 
-    def step(self):
+    def step(self, action):
+        p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
+        orn = p.getQuaternionFromEuler([2 * math.pi / 2., 0, 0.])
+        jointPoses = p.calculateInverseKinematics(self.panda, pandaEndEffectorIndex, action, orn, ll, ul,
+                                                jr, rp, maxNumIterations=20)
+
+        for i in range(pandaNumDofs):
+            p.setJointMotorControl2(self.panda, i, p.POSITION_CONTROL, jointPoses[i], force=120.)
+
         p.stepSimulation()
-        self.update_state()
-        time.sleep(1. / 240.)
+        time.sleep(1 / 240.)
 
-        alpha = 0.9
-        if self.state == 1 or self.state == 2 or self.state == 3 or self.state == 4 or self.state == 7:
-            # gripper_height = 0.034
-            self.gripper_height = alpha * self.gripper_height + (1. - alpha) * 0.03
-            if self.state == 2 or self.state == 3 or self.state == 7:
-                self.gripper_height = alpha * self.gripper_height + (1. - alpha) * 0.2
+    # def step(self, action):
+    #     p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
+    #     self.update_state()
+    #     # time.sleep(1. / 240.)
+    #     alpha = 0.9
+    #
+    #     if self.state != 0:
+    #         if self.state == 5 or self.state == 6:
+    #             finger_targets = {
+    #                 5: 0.01,
+    #                 6: 0.04
+    #             }
+    #             self.finger_target = finger_targets[self.state]
+    #         elif self.state == 1 or self.state == 2 or self.state == 3 or self.state == 4 or self.state == 7 or self.state == 8:
+    #             if self.state == 1:
+    #                 pos = [0.5, 0, 0.8]
+    #             if self.state == 2 or self.state == 3 or self.state == 4:
+    #                 indexes = {
+    #                     2: self.start_position,
+    #                     3: self.end_position,
+    #                     4: self.lego
+    #                 }
+    #                 pos, o = p.getBasePositionAndOrientation(indexes[self.state])
+    #                 current_height = p.getLinkState(self.panda, pandaEndEffectorIndex)[0][2]
+    #                 pos = [pos[0], pos[1], current_height]
+    #             if self.state == 7 or self.state == 8:
+    #                 z_pos = {
+    #                     7: 0.5,
+    #                     8: 0.8
+    #                 }
+    #                 pos = p.getLinkState(self.panda, pandaEndEffectorIndex)[0]
+    #                 pos = [pos[0], pos[1], z_pos[self.state]]
+    #
+    #             orn = p.getQuaternionFromEuler([2 * math.pi / 2., 0,  0.])
+    #             jointPoses = p.calculateInverseKinematics(self.panda, pandaEndEffectorIndex, pos, orn, ll, ul,
+    #                                                                            jr, rp, maxNumIterations=20)
+    #             self.prev_pos = pos
+    #             for i in range(pandaNumDofs):
+    #                 p.setJointMotorControl2(self.panda, i, p.POSITION_CONTROL, jointPoses[i], force=120.)
+    #     for i in [9, 10]:
+    #         p.setJointMotorControl2(self.panda, i, p.POSITION_CONTROL, self.finger_target, force=1000)
+    #
+    #     p.stepSimulation()
+    #     self.t += 1
+    #
+    #     pos_goal, _ = p.getBasePositionAndOrientation(self.lego)
+    #     pos_tsp = p.getLinkState(self.panda, pandaEndEffectorIndex)[0]
+    #
+    #     return self.t, pos_goal, pos_tsp
 
-            t = self.t
-            self.t += self.control_dt
-            pos = [self.offset[0] + 0.2 * math.sin(1.5 * t), self.offset[1] + self.gripper_height,
-                   self.offset[2] + -0.6 + 0.1 * math.cos(1.5 * t)]
-            if self.state == 3 or self.state == 4:
-                pos, o = p.getBasePositionAndOrientation(self.lego)
-                pos = [pos[0], self.gripper_height, pos[2]]
-                self.prev_pos = pos
-            if self.state == 7:
-                pos = self.prev_pos
-                diffX = pos[0] - self.offset[0]
-                diffZ = pos[2] - (self.offset[2] - 0.6)
-                self.prev_pos = [self.prev_pos[0] - diffX * 0.1, self.prev_pos[1], self.prev_pos[2] - diffZ * 0.1]
+    def render(self):
+        view_matrix = p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=[0.7,0,0.05],
+                                                            distance=.7,
+                                                            yaw=90,
+                                                            pitch=-70,
+                                                            roll=0,
+                                                            upAxisIndex=2)
+        proj_matrix = p.computeProjectionMatrixFOV(fov=60,
+                                                   aspect=float(960) /720,
+                                                   nearVal=0.1,
+                                                   farVal=100.0)
+        (_, _, px, _, _) = p.getCameraImage(width=960,
+                                            height=720,
+                                            viewMatrix=view_matrix,
+                                            projectionMatrix=proj_matrix,
+                                            renderer=p.ER_BULLET_HARDWARE_OPENGL)
 
-            orn = p.getQuaternionFromEuler([math.pi / 2., 0., 0.])
-            p.submitProfileTiming("IK")
-            jointPoses = p.calculateInverseKinematics(self.panda, pandaEndEffectorIndex, pos, orn, ll, ul,
-                                                                       jr, rp, maxNumIterations=20)
-            p.submitProfileTiming()
-            for i in range(pandaNumDofs):
-                p.setJointMotorControl2(self.panda, i, p.POSITION_CONTROL, jointPoses[i],
-                                                         force=5 * 240.)
-            # target for fingers
-        for i in [9, 10]:
-            p.setJointMotorControl2(self.panda, i, p.POSITION_CONTROL, self.finger_target,
-                                                     force=10)
-        p.submitProfileTiming()
+        rgb_array = np.array(px, dtype=np.uint8)
+        rgb_array = np.reshape(rgb_array, (720, 960, 4))
+
+        rgb_array = rgb_array[:, :, :3]
+        return rgb_array
+
+    def close(self):
+        p.disconnect()
 
 
 env = Environment()
 env.reset()
-while (1):
-    env.step()
+# while (1):
+for _ in range(100000):
+    # env.render()
+    action = env.action_space.sample()
+    print(action)
+    env.step(action)
+    # env.step(1)
+env.close()
---
+++ trainer.py
@@ -0,0 +1,430 @@
+from __future__ import annotations
+import pickle
+from model import PPOActor, PPOCritic
+from panda_gym.envs.core import RobotTaskEnv
+from torch import Tensor
+from torch.nn import MSELoss
+import numpy as np
+
+import torch
+from typing import Tuple, List
+import sys
+
+import matplotlib.pyplot as plt
+
+EpisodeMemory: Tuple[List[np.array], List[np.array], List[np.array], List[float]]
+
+
+class Trainer:
+    def __init__(
+        self,
+        env: RobotTaskEnv,
+        actor: PPOActor,
+        critic: PPOCritic,
+        timesteps: int,
+        timesteps_per_batch: int,
+        max_timesteps_per_episode: int,
+        γ: float = 0.99,
+        ε: float = 0.2,
+        # α: float = 0.005,
+        # α: float = 0.01,
+        α: float = 3e-4,
+        training_cycles_per_batch: int = 5,
+        save_every_x_timesteps: int = 50_000,
+    ):
+        self.env = env
+        self.actor = actor
+        self.critic = critic
+
+        self.timesteps = timesteps
+        self.current_timestep = 0
+        self.max_timesteps_per_episode = max_timesteps_per_episode
+        self.timesteps_per_batch = timesteps_per_batch
+        self.save_every_x_timesteps = save_every_x_timesteps
+        self.last_save = 0
+
+        # Hyperparameters
+        self.γ = γ
+        self.ε = ε
+        self.α = α
+        self.training_cycles_per_batch = training_cycles_per_batch
+
+        # Memory
+        self.total_rewards: List[float] = []
+        self.terminal_timesteps: List[int] = []
+        self.actor_losses: List[float] = []
+        self.critic_losses: List[float] = []
+
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.α)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.α)
+
+        # Initialize some status tracking memory
+        self.previous_print_length: int = 0
+        self.current_action = "Initializing"
+        self.last_save: int = 0
+
+    def print_status(self):
+        latest_reward = 0.0
+        average_reward = 0.0
+        best_reward = 0.0
+        latest_actor_loss = 0.0
+        avg_actor_loss = 0.0
+        latest_critic_loss = 0.0
+        avg_critic_loss = 0.0
+        recent_change = 0.0
+
+        if len(self.total_rewards) > 0:
+            latest_reward = self.total_rewards[-1]
+
+            last_n_episodes = 100
+            average_reward = np.mean(self.total_rewards[-last_n_episodes:])
+
+            episodes = [
+                i
+                for i in range(
+                    len(self.total_rewards[-last_n_episodes:]),
+                    min(last_n_episodes, 0),
+                    -1,
+                )
+            ]
+            coefficients = np.polyfit(
+                episodes,
+                self.total_rewards[-last_n_episodes:],
+                1,
+            )
+            recent_change = coefficients[0]
+
+            best_reward = max(self.total_rewards)
+
+        if len(self.actor_losses) > 0:
+            avg_count = 3 * self.timesteps_per_batch
+            latest_actor_loss = self.actor_losses[-1]
+            avg_actor_loss = np.mean(self.actor_losses[-avg_count:])
+            latest_critic_loss = self.critic_losses[-1]
+            avg_critic_loss = np.mean(self.critic_losses[-avg_count:])
+
+        msg = f"""
+            =========================================
+            Timesteps: {self.current_timestep:,} / {self.timesteps:,} ({round((self.current_timestep/self.timesteps)*100, 4)}%)
+            Episodes: {len(self.total_rewards):,}
+            Currently: {self.current_action}
+            Latest Reward: {round(latest_reward)}
+            Latest Avg Rewards: {round(average_reward)}
+            Recent Change: {round(recent_change, 2)}
+            Best Reward: {round(best_reward, 2)}
+            Latest Actor Loss: {round(latest_actor_loss, 4)}
+            Avg Actor Loss: {round(avg_actor_loss, 4)}
+            Latest Critic Loss: {round(latest_critic_loss, 4)}
+            Avg Critic Loss: {round(avg_critic_loss, 4)}
+            =========================================
+        """
+
+        # We print to STDERR as a hack to get around the noisy pybullet
+        # environment. Hacky, but effective if paired w/ 1> /dev/null
+        print(msg, file=sys.stderr)
+
+    def create_plot(self, filepath: str):
+        last_n_episodes = 10
+
+        episodes = [i + 1 for i in range(len(self.total_rewards))]
+        averages = [
+            np.mean(self.total_rewards[i - last_n_episodes : i])
+            for i in range(len(self.total_rewards))
+        ]
+        trend_data = np.polyfit(episodes, self.total_rewards, 1)
+        trendline = np.poly1d(trend_data)
+
+        plt.scatter(
+            episodes, self.total_rewards, color="green"
+        )  # , linestyle='None', marker='o', color='green')
+        plt.plot(episodes, averages, linestyle="solid", color="red")
+        plt.plot(episodes, trendline(episodes), linestyle="--", color="blue")
+
+        plt.title("Rewards per episode")
+        plt.ylabel("Reward")
+        plt.xlabel("Episode")
+        plt.savefig(filepath)
+
+    def save(self, directory: str):
+        """
+        save will save the models, state, and any additional
+        data to the given directory
+        """
+        self.last_save = self.current_timestep
+
+        self.actor.save(f"{directory}/actor.pth")
+        self.critic.save(f"{directory}/critic.pth")
+        self.create_plot(f"{directory}/rewards.png")
+
+        # Now save the trainer's state data
+        data = {
+            "timesteps": self.timesteps,
+            "current_timestep": self.current_timestep,
+            "max_timesteps_per_episode": self.max_timesteps_per_episode,
+            "timesteps_per_batch": self.timesteps_per_batch,
+            "save_every_x_timesteps": self.save_every_x_timesteps,
+            "γ": self.γ,
+            "ε": self.ε,
+            "α": self.α,
+            "training_cycles_per_batch": self.training_cycles_per_batch,
+            "total_rewards": self.total_rewards,
+            "terminal_timesteps": self.terminal_timesteps,
+            "actor_losses": self.actor_losses,
+            "critic_losses": self.critic_losses,
+        }
+        pickle.dump(data, open(f"{directory}/state.data", "wb"))
+
+    def load(self, directory: str):
+        """
+        Load will load the models, state, and any additional
+        data from the given directory
+        """
+        # Load our models first; they're the simplest
+        self.actor.load(f"{directory}/actor.pth")
+        self.critic.load(f"{directory}/critic.pth")
+
+        data = pickle.load(open(f"{directory}/state.data", "rb"))
+
+        self.timesteps = data["timesteps"]
+        self.current_timestep = data["current_timestep"]
+        self.last_save = self.current_timestep
+        self.max_timesteps_per_episode = data["max_timesteps_per_episode"]
+        self.timesteps_per_batch = data["timesteps_per_batch"]
+        self.save_every_x_timesteps = data["save_every_x_timesteps"]
+
+        # Hyperparameters
+        self.γ = data["γ"]
+        self.ε = data["ε"]
+        self.α = data["α"]
+        self.training_cycles_per_batch = data["training_cycles_per_batch"]
+
+        # Memory
+        self.total_rewards = data["total_rewards"]
+        self.terminal_timesteps = data["terminal_timesteps"]
+        self.actor_losses = data["actor_losses"]
+        self.critic_losses = data["critic_losses"]
+
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.α)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.α)
+
+    def run_episode(self) -> EpisodeMemory:
+        """
+        run_episode runs a singular episode and returns the results
+        """
+        observation, _ = self.env.reset()
+        if isinstance(observation, dict):
+            observation = observation["observation"]
+
+        timesteps = 0
+        observations: List[np.ndarray] = []
+        actions: List[np.ndarray] = []
+        log_probabilities: List[float] = []
+        rewards: List[float] = []
+
+        while True:
+            timesteps += 1
+
+            observations.append(observation)
+            action_distribution = self.actor(observation)
+            action = action_distribution.sample()
+            log_probability = action_distribution.log_prob(action).detach().numpy()
+            action = action.detach().numpy()
+            observation, reward, terminated, _, _ = self.env.step(action)
+            if isinstance(observation, dict):
+                observation = observation["observation"]
+
+            actions.append(action)
+            log_probabilities.append(log_probability)
+            rewards.append(reward)
+
+            if timesteps >= self.max_timesteps_per_episode:
+                terminated = True
+
+            if terminated:
+                break
+
+        # Calculate the discounted rewards for this episode
+        discounted_rewards: List[float] = self.calculate_discounted_rewards(rewards)
+
+        # Get the terminal reward and record it for status tracking
+        self.total_rewards.append(sum(rewards))
+
+        return observations, actions, log_probabilities, discounted_rewards
+
+    def rollout(self) -> EpisodeMemory:
+        """
+        rollout will perform a rollout of the environment and
+        return the memory of the episode with the current
+        actor model
+        """
+        observations: List[np.ndarray] = []
+        log_probabilities: List[float] = []
+        actions: List[float] = []
+        rewards: List[float] = []
+
+        while len(observations) < self.timesteps_per_batch:
+            self.current_action = "Rollout"
+            obs, chosen_actions, log_probs, rwds = self.run_episode()
+            # Combine these arrays into our overall batch
+            observations += obs
+            actions += chosen_actions
+            log_probabilities += log_probs
+            rewards += rwds
+
+            # Increment our count of timesteps
+            self.current_timestep += len(obs)
+
+            self.print_status()
+
+        # We need to trim the batch memory to the batch size
+        observations = observations[: self.timesteps_per_batch]
+        actions = actions[: self.timesteps_per_batch]
+        log_probabilities = log_probabilities[: self.timesteps_per_batch]
+        rewards = rewards[: self.timesteps_per_batch]
+
+        return observations, actions, log_probabilities, rewards
+
+    def calculate_discounted_rewards(self, rewards: List[float]) -> List[float]:
+        """
+        calculated_discounted_rewards will calculate the discounted rewards
+        of each timestep of an episode given its initial rewards and episode
+        length
+        """
+        discounted_rewards: List[float] = []
+        discounted_reward: float = 0
+        for reward in reversed(rewards):
+            discounted_reward = reward + (self.γ * discounted_reward)
+            # We insert here to append to the front as we're calculating
+            # backwards from the end of our episodes
+            discounted_rewards.insert(0, discounted_reward)
+
+        return discounted_rewards
+
+    def calculate_normalized_advantage(
+        self, observations: Tensor, rewards: Tensor
+    ) -> Tensor:
+        """
+        calculate_normalized_advantage will calculate the normalized
+        advantage of a given batch of episodes
+        """
+        V = self.critic(observations).detach().squeeze()
+
+        # Now we need to calculate our advantage and normalize it
+        advantage = Tensor(np.array(rewards, dtype="float32")) - V
+        normalized_advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-8)
+
+        return normalized_advantage
+
+    def training_step(
+        self,
+        observations: Tensor,
+        actions: Tensor,
+        log_probabilities: Tensor,
+        rewards: Tensor,
+        normalized_advantage: Tensor,
+    ) -> Tuple[float, float]:
+        """
+        training_step will perform a single epoch of training for the
+        actor and critic model
+
+        Returns the loss for each model at the end of the step
+        """
+        # Get our output for the current actor given our log
+        # probabilities
+        current_action_distributions = self.actor(observations)
+        current_log_probabilities = current_action_distributions.log_prob(actions)
+
+        # We are calculating the ratio as defined by:
+        #
+        #   π_θ(a_t | s_t)
+        #   --------------
+        #   π_θ_k(a_t | s_t)
+        #
+        # ...where our originaly utilized log probabilities
+        # are π_θ_k and our current model is creating π_θ. We
+        # use the log probabilities and subtract, then raise
+        # e to the power of the results to simplify the math
+        # for back propagation/gradient descent.
+        # Note that we have a log probability matrix of shape
+        # (batch size, number of actions), where we're expecting
+        # (batch size, 1). We sum our logs as the log(A + B) =
+        # log(A) + log(B).
+        # log_probabilities = Tensor(np.array(log_probabilities, dtype=np.float32))
+        # log_probabilities = torch.sum(log_probabilities, dim=-1)
+        # current_log_probabilities = torch.sum(current_log_probabilities, dim=-1)
+        ratio = torch.exp(current_log_probabilities - log_probabilities)
+
+        # Now we calculate the actor loss for this step
+        actor_loss = -torch.min(
+            ratio * normalized_advantage,
+            torch.clamp(ratio, 1 - self.ε, 1 + self.ε) * normalized_advantage,
+        ).mean()
+
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # Now we do a training step for the critic
+
+        # Calculate what the critic current evaluates our states as.
+        # First we have the critic evaluate all observation states,
+        # then compare it ot the collected rewards over that time.
+        # We will convert our rewards into a known tensor
+        V = self.critic(observations)
+        reward_tensor = Tensor(rewards).unsqueeze(-1)
+        critic_loss = MSELoss()(V, reward_tensor)
+
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        return actor_loss.item(), critic_loss.item()
+
+    def train(self):
+        """
+        train will train the actor and critic models with the
+        given training state
+        """
+        while self.current_timestep <= self.timesteps:
+            # Perform a rollout to get our next training
+            # batch
+            observations, actions, log_probabilities, rewards = self.rollout()
+
+            # Finally, convert these to numpy arrays and then to tensors
+            observations = Tensor(np.array(observations, dtype=np.float32))
+            actions = Tensor(np.array(actions, dtype=np.float32))
+            log_probabilities = Tensor(np.array(log_probabilities, dtype=np.float32))
+            rewards = Tensor(np.array(rewards, dtype=np.float32))
+
+            # Perform our training steps
+            for c in range(self.training_cycles_per_batch):
+                self.current_action = (
+                    f"Training Cycle {c+1}/{self.training_cycles_per_batch}"
+                )
+                self.print_status()
+
+                # Calculate our losses
+                normalized_advantage = self.calculate_normalized_advantage(
+                    observations, rewards
+                )
+                actor_loss, critic_loss = self.training_step(
+                    observations,
+                    actions,
+                    log_probabilities,
+                    rewards,
+                    normalized_advantage,
+                )
+                self.actor_losses.append(actor_loss)
+                self.critic_losses.append(critic_loss)
+
+            # Every X timesteps, save our current status
+            if self.current_timestep - self.last_save >= self.save_every_x_timesteps:
+                self.current_action = "Saving"
+                self.print_status()
+                self.save("training")
+
+        print("")
+        print("Training complete!")
+
+        # Save our results
+        self.save("training")
